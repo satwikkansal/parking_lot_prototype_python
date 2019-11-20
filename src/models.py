ecfebb962f802ff9497e1950ba4b5a5c453ef679
Satwik <satwikkansal@gmail.com>
2019-11-20 22:14:48 +0530

    Add dummy interface methods for the model classes.

--- src/models.py
+++ src/models.py
@@ -15,6 +15,50 @@ class ParkingLot:
 
     def __post_init__(self):
         self.available_slots = [True] * self.size
+        # Using these as an index for fast execution of color
+        # and registration lookups
+        self.color_index = {}
+        self.car_index = {}
+
+    def park(self, car: Car):
+        """
+        Parks the car to the next available slot if present. Updates the
+        `color_index` and `car_index` accordingly.
+        :param car: Instance of `Car`
+        :return: Ticket is the car is slot is available, None otherwise.
+        """
+        pass
+
+    def get_cars_by_color(self, color: str):
+        """
+        :param color: Color string
+        :return: Returns all the parked cars with given color string.
+        """
+        tickets = self.color_index.get(color)
+        if tickets:
+            return [ticket.car for ticket in tickets]
+
+    def get_slots_by_color(self, color: str):
+        """
+        :param color: Color string
+        :return: Returns all the slot numbers with given `color` car parked.
+        """
+        tickets = self.color_index.get(color)
+        if tickets:
+            return [ticket.car.color for ticket in tickets]
+
+    def get_slot_number_for_car(self, registration_number:str):
+        """
+        :param registration_number:
+        :return: An integer representing the slot number where the car is parked.
+        """
+        return self.car_index.get(registration_number)
+
+    def get_parked_cars_information(self):
+        """
+        :return: The `car_index` maintained by the object instance.
+        """
+        return self.car_index
 
 
 @dataclass
