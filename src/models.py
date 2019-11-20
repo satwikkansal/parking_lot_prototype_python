3b26b71ccc6464b1428ac1b41d8822e71b33e753
Satwik <satwikkansal@gmail.com>
2019-11-20 23:31:57 +0530

    models.py: Implement methods to pass the test cases

--- src/models.py
+++ src/models.py
@@ -1,33 +1,90 @@
+from collections import defaultdict
 from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(frozen=True, eq=True)
 class Car:
     registration_number: str
     color: str
+
+
+@dataclass(frozen=True, eq=True)
+class Ticket:
+    """
+    This class will act as glue (encapsulates relationship) between ParkingLot and a Car
+    """
+    car: Car
+    slot: int
 
 
 @dataclass
 class ParkingLot:
     size: int
     next_available: int = 0
-    available_slots: list = field(init=False)
+    slots: list = field(init=False)
 
     def __post_init__(self):
-        self.available_slots = [True] * self.size
+        self.slots = [None] * self.size
         # Using these as an index for fast execution of color
         # and registration lookups
-        self.color_index = {}
+        self.color_index = defaultdict(set)
         self.car_index = {}
+
+    def is_full(self):
+        """
+        :return: True if there are no vacant slots, False otherwise.
+        """
+        return len(self.car_index) == self.size
+
+    def find_next_available_slot(self):
+        """
+        :return: Slot number of the nearest available slot. `None` if
+                 no slot is available.
+        """
+        if self.is_full(): return None
+
+        for slot_number, slot_content in enumerate(self.slots):
+            if slot_content is None: return slot_number
 
     def park(self, car: Car):
         """
         Parks the car to the next available slot if present. Updates the
         `color_index` and `car_index` accordingly.
         :param car: Instance of `Car`
-        :return: Ticket is the car is slot is available, None otherwise.
+        :return: Ticket if the car is slot is available, None otherwise.
         """
-        pass
+        next_available_slot = self.find_next_available_slot()
+        if next_available_slot is None:
+            return None
+
+        # Generate a ticket
+        ticket = Ticket(car, next_available_slot)
+        # Park the car
+        self.slots[next_available_slot] = car
+
+        # Update the indices
+        self.car_index[car.registration_number] = ticket
+        self.color_index[car.color].add(ticket)
+        return ticket
+
+    def evict(self, slot_number: int):
+        """
+        Creates vacancy for a new car at the given slot number.
+        :param slot_number: slot number in the parking lot
+        :return: Car object if present at the indicated slot, None otherwise
+        """
+        if slot_number >= self.size or self.slots[slot_number] is None:
+            return None
+
+        car = self.slots[slot_number]
+
+        # Create vacancy
+        self.slots[slot_number] = None
+        # Update the indices
+        ticket = self.car_index[car.registration_number]
+        del self.car_index[car.registration_number]
+        self.color_index[car.color].remove(ticket)
+        return car
 
     def get_cars_by_color(self, color: str):
         """
@@ -37,6 +94,7 @@ class ParkingLot:
         tickets = self.color_index.get(color)
         if tickets:
             return [ticket.car for ticket in tickets]
+        else: return []
 
     def get_slots_by_color(self, color: str):
         """
@@ -45,23 +103,19 @@ class ParkingLot:
         """
         tickets = self.color_index.get(color)
         if tickets:
-            return [ticket.car.color for ticket in tickets]
+            return [ticket.slot for ticket in tickets]
+        else: return []
 
-    def get_slot_number_for_car(self, registration_number:str):
+    def get_slot_number_for_car(self, registration_number: str):
         """
         :param registration_number:
         :return: An integer representing the slot number where the car is parked.
         """
-        return self.car_index.get(registration_number)
+        ticket = self.car_index.get(registration_number)
+        if ticket: return ticket.slot
 
     def get_parked_cars_information(self):
         """
         :return: The `car_index` maintained by the object instance.
         """
         return self.car_index
-
-
-@dataclass
-class Ticket:
-    car: Car
-    slot: int
