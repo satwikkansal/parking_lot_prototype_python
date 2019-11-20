0b295cabc1db680c748aa4c57bffcb94bd57415c
Satwik <satwikkansal@gmail.com>
2019-11-20 21:47:50 +0530

    enums.py: Fix typo

--- src/enums.py
+++ src/enums.py
@@ -11,5 +11,5 @@ class Command(Enum):
     STATUS = 'status'
     SLOTS_BY_COLOUR = 'slot_numbers_for_cars_with_colour'
     CARS_BY_COLOUR = 'registration_numbers_for_cars_with_colour'
-    SLOT_FOR_CA_NUMBER = 'slot_number_for_registration_number'
+    SLOT_FOR_CAR_NUMBER = 'slot_number_for_registration_number'
     EXIT = 'exit'
